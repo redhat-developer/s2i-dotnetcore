c8fd0da755491a54502774271f388aeb00189fa8
Severin Gehwolf <sgehwolf@redhat.com>
2016-12-08 13:28:19 -0500

    Add tests for CLI apps and top-level build.sh
    Also add top-level build.sh so as to be able to drive
    building/testing of all versions.
    
    Example building all images, running tests on them:
    
      $ sudo ./build.sh
    
    Example building 1.0 image and running tests on it:
    
      $ sudo VERSIONS=1.0 ./build.sh

---
+++ build.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+#
+# Build Docker images and run tests.
+#
+# PRE: Docker daemon running, s2i binary available.
+#
+# Supported environment variables:
+#
+# -----------------------------------------------------
+# VERSIONS     The list of versions to build/test.
+#              Defaults to all versions. i.e "1.0 1.1".
+# -----------------------------------------------------
+#
+# Usage:
+#       $ sudo ./build.sh
+#       $ sudo VERSIONS=1.0 ./build.sh
+#
+
+version_no_dot() {
+  local version=$1
+  echo ${version} | sed 's/\.//g'
+}
+
+image_exists() {
+  docker inspect $1 &>/dev/null
+}
+
+check_result_msg() {
+  local retval=$1
+  local msg=$2
+  if [ ${retval} -ne 0 ]; then
+    echo 1>&2 "${msg}"
+    exit 1
+  fi
+}
+
+VERSIONS="${VERSIONS:-1.0 1.1}"
+
+for v in ${VERSIONS}; do
+  v_no_dot="$( version_no_dot ${v} )"
+  img_name="dotnet/dotnetcore-${v_no_dot}-rhel7"
+  pushd ${v} &>/dev/null
+    if ! image_exists ${img_name}; then
+      echo "Building Docker image ${img_name} ..."
+      docker build -f Dockerfile.rhel7 -t ${img_name} .
+      check_result_msg $? "Building Docker image ${img_name} FAILED!"
+    fi
+    echo "Running tests on image ${img_name} ..."
+    ./test/run
+    check_result_msg $? "Tests for image ${img_name} FAILED!"
+  popd &>/dev/null
+done
+
+echo "ALL builds and tests were successful!"
+exit 0
