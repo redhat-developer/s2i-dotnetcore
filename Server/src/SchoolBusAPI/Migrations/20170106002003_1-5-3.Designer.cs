f4fa5613d9fb7a8b10082edfc2560fb930d4a590
georgelwalker <gwalker@escapesystems.com>
2017-01-05 16:23:18 -0800

    SB-87 - Adjustment to primary key naming convention

--- Server/src/SchoolBusAPI/Migrations/20170106002003_1-5-3.Designer.cs
+++ Server/src/SchoolBusAPI/Migrations/20170106002003_1-5-3.Designer.cs
@@ -8,9 +8,10 @@ using SchoolBusAPI.Models;
 namespace SchoolBusAPI.Migrations
 {
     [DbContext(typeof(DbAppContext))]
-    partial class DbAppContextModelSnapshot : ModelSnapshot
+    [Migration("20170106002003_1-5-3")]
+    partial class _153
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
             modelBuilder
                 .HasAnnotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.SerialColumn)
