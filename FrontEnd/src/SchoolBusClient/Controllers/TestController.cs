af8bf524b7d674f6445b14572fc36b73707c0f3d
Wade Barnes <wade.barnes@quartech.com>
2017-01-07 15:54:15 -0800

    SB-108 - Fix APIs for echoing headers.

--- FrontEnd/src/SchoolBusClient/Controllers/TestController.cs
+++ FrontEnd/src/SchoolBusClient/Controllers/TestController.cs
@@ -1,22 +1,13 @@
-/*
- * REST API Documentation for the MOTI School Bus Application
- *
- * The School Bus application tracks that inspections are performed in a timely fashion. For each school bus the application tracks information about the bus (including data from ICBC, NSC, etc.), it's past and next inspection dates and results, contacts, and the inspector responsible for next inspecting the bus.
- *
- * OpenAPI spec version: v1
- * 
- * 
- */
-
-using Microsoft.AspNetCore.Http;
+﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System.Collections.Generic;
 using System.Text;
 
-namespace SchoolBusAPI.Controllers
+namespace SchoolBusClient.Controllers
 {
-    [Route("api/test")]
-    public class TestApiController : Controller
+    [Route("test")]
+    [Route("schoolbus/test")]
+    public class TestController : Controller
     {
         /// <summary>
         /// Echos request headers
@@ -26,7 +17,7 @@ namespace SchoolBusAPI.Controllers
         /// </returns>
         [HttpGet]
         [Route("headers")]
-        [Produces("text/html")]        
+        [Produces("text/html")]
         public virtual IActionResult EchoHeaders()
         {
             string content = HeadersToHtml(Request.Headers);
