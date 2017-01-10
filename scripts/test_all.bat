fd5f6b7c3ae5c894d646b6063e1da17e6d94a501
georgelwalker <gwalker@escapesystems.com>
2017-01-10 09:08:44 -0800

    SB-93 updated test script

--- scripts/test_all.bat
+++ scripts/test_all.bat
@@ -13,19 +13,16 @@ call load.bat "..\ApiSpec\TestData\OwnerContacts\OwnerContacts_Contact.json" api
 call load.bat "..\ApiSpec\TestData\Owners\Owners_SBOwner.json" api/schoolbusowners/bulk "%TARGET_SERVER%"
 call load.bat "..\ApiSpec\TestData\SchoolBus\SchoolBus_SchoolBus.json" api/schoolbuses/bulk "%TARGET_SERVER%"
 call load.bat "..\ApiSpec\TestData\users\users_user.json" api/users/bulk "%TARGET_SERVER%"
-call load.bat "..\ApiSpec\TestData\userGroup\userGroup_userGroup.json" api/users/bulk "%TARGET_SERVER%"
 
 rem needs endpoint
-rem call load.bat "..\ApiSpec\TestData\users\users_user.json" api/users/bulk "%TARGET_SERVER%"
+rem call load.bat "..\ApiSpec\TestData\userRole\userRole" api/users/bulk "%TARGET_SERVER%"
+rem call load.bat "..\ApiSpec\TestData\userGroup\userGroup_userGroup.json" api/usergroups/bulk "%TARGET_SERVER%"
 
-rem needs endpoint
 rem call load.bat "..\ApiSpec\TestData\rolepermission\rolepermission_RP.json" api/groups/bulk %TARGET_SERVER%
 
-rem needs endpoint
-rem call load.bat "..\ApiSpec\TestData\CCW\CCW_CCW.json" api/ccwdata/bulk http://localhost:53477
+rem call load.bat "..\ApiSpec\TestData\CCW\CCW_CCW.json" api/ccwdata/bulk %TARGET_SERVER%
 
-rem needs endpoint
-rem call load.bat "..\ApiSpec\TestData\group\group_Group.json" api/groups/bulk http://localhost:53477
+rem call load.bat "..\ApiSpec\TestData\group\group_Group.json" api/groups/bulk %TARGET_SERVER%
 
 
 
