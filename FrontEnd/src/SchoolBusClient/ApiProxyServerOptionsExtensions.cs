2369eaf0be09a767421e95c850f77b809219ff16
Wade Barnes <wade.barnes@shaw.ca>
2017-01-13 16:05:48 -0800

    HETS-26 - Added version APIs
    Added APIs that provide details application and database version information.

---
+++ FrontEnd/src/SchoolBusClient/ApiProxyServerOptionsExtensions.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Options;
+using System;
+
+namespace SchoolBusClient
+{
+    public static class ApiProxyServerOptionsExtensions
+    {
+        public static Uri ToUri(this ApiProxyServerOptions options)
+        {
+            int portNumber = 80;
+            int.TryParse(options.Port, out portNumber);
+            UriBuilder uriBuilder = new UriBuilder(options.Scheme, options.Host, portNumber);
+            return uriBuilder.Uri;
+        }
+
+
+        public static IOptions<ProxyOptions> ToProxyOptions(this ApiProxyServerOptions options)
+        {
+            ProxyOptions proxyOptions = new ProxyOptions()
+            {
+                Host = options.Host,
+                Port = options.Port,
+                Scheme = options.Scheme
+            };
+
+            return Options.Create(proxyOptions);
+        }
+    }
+}
--- FrontEnd/src/SchoolBusClient/Handlers/ApiProxyMiddleware.cs
+++ FrontEnd/src/SchoolBusClient/Handlers/ApiProxyMiddleware.cs
@@ -1,32 +1,19 @@
-﻿using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Http;
+﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Proxy;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Options;
 using System;
 using System.Threading.Tasks;
 
-
 namespace SchoolBusClient.Handlers
 {
-    // ToDo:
-    // - Replace MIDDLEWARE_NAME environment variable with variables the can be used with ApiServerOptions:
-    // -- ApiProxyServer:Scheme
-    // -- ApiProxyServer:Host
-    // -- ApiProxyServer:Port
-    // - Remove use of IConfiguration and MIDDLEWARE_NAME environment variable 
     public class ApiProxyMiddleware
     {
         private static readonly string _apiPathKey = "/api/";
-        private readonly IOptions<ApiProxyServerOptions> _apiServerOptions;
-        private readonly IConfiguration _configuration;
         private readonly Microsoft.AspNetCore.Proxy.ProxyMiddleware _proxy;
 
-        public ApiProxyMiddleware(RequestDelegate next, IConfiguration configuration, IOptions<ApiProxyServerOptions> apiServerOptions)
+        public ApiProxyMiddleware(RequestDelegate next, IOptions<ApiProxyServerOptions> apiServerOptions)
         {
-            _apiServerOptions = apiServerOptions;
-            _configuration = configuration;
-            _proxy = new ProxyMiddleware(next, this.ProxyOptions);
+            _proxy = new ProxyMiddleware(next, apiServerOptions.Value.ToProxyOptions());
         }
 
         /// <summary>
@@ -40,39 +27,6 @@ namespace SchoolBusClient.Handlers
             int indexOfApi = requestPath.IndexOf(_apiPathKey);
             context.Request.Path = requestPath.Remove(0, indexOfApi);
             await _proxy.Invoke(context);
-        }
-
-        private IOptions<ProxyOptions> ProxyOptions
-        {
-            get
-            {
-                ProxyOptions options = null;
-                string apiServerUri = _configuration["MIDDLEWARE_NAME"];
-                if (apiServerUri != null)
-                {
-                    string[] apiServerUriParts = apiServerUri.Split(':');
-                    string host = apiServerUriParts[0];
-                    string port = apiServerUriParts.Length > 1 ? apiServerUriParts[1] : null;
-
-                    options = new ProxyOptions()
-                    {
-                        Scheme = "http",
-                        Host = host,
-                        Port = port
-                    };
-                }
-                else
-                {
-                    options = new ProxyOptions()
-                    {
-                        Scheme = _apiServerOptions.Value.Scheme,
-                        Host = _apiServerOptions.Value.Host,
-                        Port = _apiServerOptions.Value.Port
-                    };
-                }
-
-                return Options.Create(options);
-            }
         }
 
         public static bool IsApiPath(HttpContext httpContext)
