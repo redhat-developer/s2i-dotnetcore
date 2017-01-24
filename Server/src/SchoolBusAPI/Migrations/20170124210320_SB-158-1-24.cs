d62880c9eb27fd1f3b7b2ff09a2f4f0891ae7e19
georgelwalker <gwalker@escapesystems.com>
2017-01-24 13:03:52 -0800

    SB-158 additional files

---
+++ Server/src/SchoolBusAPI/Migrations/20170124210320_SB-158-1-24.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SchoolBusAPI.Migrations
+{
+    public partial class SB158124 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
