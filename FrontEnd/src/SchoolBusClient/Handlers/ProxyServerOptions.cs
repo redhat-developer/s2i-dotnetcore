f501aa4fe25a7221b5a0c086d8f7d1b0bda0f67c
Wade Barnes <wade.barnes@shaw.ca>
2017-01-26 09:58:18 -0800

    HETS-75 - Refactor proxy classes.
    Created a set of bases classes that make it easy to wire up new proxy routes that have their own configuration, ensuring complete separation of concern between proxies.
    Separated the existing proxy routes into their own implementation and configuration.
    Done in preparation for the next time another proxy route is needed; for instance for hangfire.

---
+++ FrontEnd/src/SchoolBusClient/Handlers/ProxyServerOptions.cs
@@ -0,0 +1,13 @@
+﻿namespace SchoolBusClient.Handlers
+{
+    public class ProxyServerOptions
+    {
+        public ProxyServerOptions()
+        {}
+
+        public string Scheme { get; set; }
+        public string Host { get; set; }
+        public string Port { get; set; }
+        public string PathKey { get;  set; }
+    }
+}
---
+++ FrontEnd/src/SchoolBusClient/Handlers/SwaggerProxyServerOptions.cs
@@ -0,0 +1,13 @@
+﻿namespace SchoolBusClient.Handlers
+{
+    public class SwaggerProxyServerOptions : ProxyServerOptions
+    {
+        public SwaggerProxyServerOptions()
+        {
+            Scheme = "http";
+            Host = "localhost";
+            Port = "80";
+            PathKey = "/swagger/";
+        }
+    }
+}
