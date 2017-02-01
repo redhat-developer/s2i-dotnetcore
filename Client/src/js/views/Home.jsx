08d8c5ee4dfc051587ea91e48d8f1e69ddd86d07
Randy Posynick <randy.posynick@gmail.com>
2017-02-01 13:12:42 -0800

    SB-166 Added Home screen logo.

--- Client/src/js/views/Home.jsx
+++ Client/src/js/views/Home.jsx
@@ -11,15 +11,23 @@ var Home = React.createClass({
 
   render: function() {
     return <div id="home">
-      <PageHeader>{this.props.currentUser.fullName}<br/>{this.props.currentUser.districtName} District</PageHeader>
-      <Row>
+      <PageHeader>
+        <Row>
+          <Col md={8}>
+            {this.props.currentUser.fullName}<br/>{this.props.currentUser.districtName} District
+          </Col>
+          <Col md={4}>
+            <img id="home-logo" src="../images/logo.png"/>
+          </Col>
+        </Row>
+      </PageHeader>
+      <Row id="home-summary">
         <Col md={8}>
           <h2>You have <a href="#/school-buses">{this.props.currentUser.overdueInspections}</a> overdue inspections</h2>
           <h2>You have <a href="#/school-buses">{this.props.currentUser.scheduledReinspections}</a> re-inspections scheduled</h2>
           <h2>You have <a href="#/school-buses">{this.props.currentUser.dueNextMonthInspections}</a> inspections coming due in the next month</h2>
         </Col>
         <Col md={4}>
-
         </Col>
       </Row>
     </div>;
