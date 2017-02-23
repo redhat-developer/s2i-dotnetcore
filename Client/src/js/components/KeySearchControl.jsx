ef9e5367e5f46a2d7a79af9ea1c6c8b8dcd4c3ee
Randy Posynick <randy.posynick@gmail.com>
2017-02-22 23:39:46 -0800

    SB-223 HETS-136 Tweak to KeySearchControl.

--- Client/src/js/components/KeySearchControl.jsx
+++ Client/src/js/components/KeySearchControl.jsx
@@ -1,7 +1,15 @@
 import React from 'react';
 
+import _ from 'lodash';
+
 import SearchControl from '../components/SearchControl.jsx';
 
+
+const KEY_SEARCH_ITEMS = [
+  { id: 'regi',  name: 'Registration' },
+  { id: 'vin',   name: 'VIN' },
+  { id: 'plate', name: 'Plate' },
+];
 
 var KeySearchControl = React.createClass({
   propTypes: {
@@ -10,8 +18,10 @@ var KeySearchControl = React.createClass({
   },
 
   getInitialState() {
+    var item = _.find(KEY_SEARCH_ITEMS, { name: this.props.search.keySearchField });
+
     return {
-      key: this.props.search.keySearchField || 'regi',
+      key: item ? item.id : KEY_SEARCH_ITEMS[0].id,
       text: this.props.search.keySearchText || '',
       params: this.props.search.keySearchParams || null,
     };
@@ -21,8 +31,10 @@ var KeySearchControl = React.createClass({
     // update local state
     this.setState(state, () => {
       // then update parent state
+      var item = _.find(KEY_SEARCH_ITEMS, { id: this.state.key });
+
       this.props.updateState({
-        keySearchField: this.state.key,
+        keySearchField: item ? item.name : '',
         keySearchText: this.state.text,
         keySearchParams: this.state.params,
       });
@@ -30,13 +42,7 @@ var KeySearchControl = React.createClass({
   },
 
   render() {
-    return <SearchControl { ...this.props } search={ this.state } updateState={ this.updated }
-      items={[
-        { id: 'regi',  name: 'Registration' },
-        { id: 'vin',   name: 'VIN' },
-        { id: 'plate', name: 'Plate' },
-      ]}
-    />;
+    return <SearchControl { ...this.props } search={ this.state } updateState={ this.updated } items={ KEY_SEARCH_ITEMS }/>;
   },
 });
 
