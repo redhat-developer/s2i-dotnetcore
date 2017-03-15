1ad6f52e0b7d4db77dde8102f1cefda374a6f10a
GeorgeWalker <gwalker@escapesystems.com>
2017-03-15 16:51:59 -0700

    HETS-79 simplified backup process

--- Backup/backup.sh
+++ Backup/backup.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
-BACKUP_DIR=/backups/
+
+while true; do
+
+# first cull backups older than 10 days.
+find $FINAL_BACKUP_DIR"*" -type d -ctime +10 | xargs rm -rf
 
 FINAL_BACKUP_DIR=$BACKUP_DIR"`date +\%Y-\%m-\%d-%H`/"
-DBFILE=$FINAL_BACKUP_DIR"$POSTGRESQL_DATABASE"
+DBFILE=$FINAL_BACKUP_DIR"$POSTGRESQL_DATABASE`date +\%Y-\%m-\%d-%H-%M`"
 echo "Making backup directory in $FINAL_BACKUP_DIR"
  
 if ! mkdir -p $FINAL_BACKUP_DIR; then
@@ -11,13 +15,16 @@ if ! mkdir -p $FINAL_BACKUP_DIR; then
 fi;
 
 PGPASSWORD=$POSTGRESQL_PASSWORD
-
 export PGPASSWORD
 
 if ! /opt/rh/rh-postgresql94/root/usr/bin/pg_dump -Fp -h "$DATABASE_SERVICE_NAME" -U "$POSTGRESQL_USER" "$POSTGRESQL_DATABASE" | gzip > $DBFILE.sql.gz.in_progress; then
-	echo "[!!ERROR!!] Failed to produce plain backup database $POSTGRESQL_DATABASE" 
+	echo "[!!ERROR!!] Failed to backup database $POSTGRESQL_DATABASE" 
 else
 	mv $DBFILE.sql.gz.in_progress $DBFILE.sql.gz
+	echo "Database backup written to $DBFILE.sql.gz"
 fi;
 
-echo "Backup complete"
+# 24 hrs
+sleep 1d
+
+done
