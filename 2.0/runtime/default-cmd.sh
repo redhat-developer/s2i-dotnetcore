a5240e4298ee1998edc1abba035a5f6ddde9896d
Tom Deseyn <tom.deseyn@gmail.com>
2017-08-09 08:02:09 -0400

    PR feedback

--- 2.0/runtime/default-cmd.sh
+++ 2.0/runtime/default-cmd.sh
@@ -3,5 +3,5 @@
 cat <<EOF
 This is a runtime image for .NET Core ${DOTNET_CORE_VERSION}.
 
-See the documentation at https://github.com/redhat-developer/s2i-dotnetcore.
+See the documentation at https://github.com/redhat-developer/s2i-dotnetcore/tree/master/${DOTNET_CORE_VERSION}/runtime.
 EOF
