4759b50add74a2871afcefd5e94d2db0abdb80a1
yudhiwidyatama <yudhi.widyatama@gmail.com>
2017-11-07 20:56:32 +0700

    Dockerfile for dotnet 1.0 on Centos 7 (#142)
    * Dockerfile for dotnet 1.0 on Centos 7
    
    * adapt test for dotnetcore 1.0 on centos7

--- build.sh
+++ build.sh
@@ -81,7 +81,7 @@ if ! [[ `grep "Red Hat Enterprise Linux" /etc/redhat-release` ]]; then
 fi
 
 if [ "$BUILD_CENTOS" = "true" ]; then
-  VERSIONS="${VERSIONS:-2.0}"
+  VERSIONS="${VERSIONS:-1.0 2.0}"
   image_os="centos7"
   image_prefix="dotnet"
   docker_filename="Dockerfile"
