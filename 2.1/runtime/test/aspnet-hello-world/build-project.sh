e2812367fc22d7df881a0ec05c8d1f905d2258e7
Tom Deseyn <tom.deseyn@gmail.com>
2018-04-25 17:02:29 +0200

    2.1 initial (#174)

--- 2.1/runtime/test/aspnet-hello-world/build-project.sh
+++ 2.1/runtime/test/aspnet-hello-world/build-project.sh
@@ -3,6 +3,6 @@
 rm -rf bin obj
 
 dotnet restore -r rhel.7-x64
-dotnet publish -f netcoreapp2.0 -c Release -r rhel.7-x64 --self-contained false /p:PublishWithAspNetCoreTargetManifest=false
+dotnet publish -f netcoreapp2.1 -c Release -r rhel.7-x64 --self-contained false /p:PublishWithAspNetCoreTargetManifest=false
 
-tar -czvf app.tar.gz -C bin/Release/netcoreapp2.0/rhel.7-x64/publish .
+tar -czvf app.tar.gz -C bin/Release/netcoreapp2.1/rhel.7-x64/publish .
