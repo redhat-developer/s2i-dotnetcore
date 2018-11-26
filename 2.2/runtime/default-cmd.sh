7b118dc0a9777e4358fe0cb8ab6e01d45d283c0b
Tom Deseyn <tom.deseyn@gmail.com>
2018-11-26 14:21:07 +0100

    2.2: runtime image: support building from pre-compiled applications

--- 2.2/runtime/default-cmd.sh
+++ 2.2/runtime/default-cmd.sh
@@ -4,4 +4,6 @@ cat <<EOF
 This is a runtime image for .NET Core ${DOTNET_CORE_VERSION}.
 
 See the documentation at https://github.com/redhat-developer/s2i-dotnetcore/tree/master/${DOTNET_CORE_VERSION}/runtime.
+
+This image can also be used as an S2I image for pre-compiled applications. Run 's2i usage <image>' for more information.
 EOF
