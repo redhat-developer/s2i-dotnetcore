c9a61946712dbe4971db78174e21cf41e79e64d2
Young-Jin Chung <Young-Jin.Chung@gov.bc.ca>
2020-07-16 13:18:00 -0700

    Fixed build issue

--- CCW/src/Schoolbus.WS.CcwFacade/Service/Config.cs
+++ CCW/src/Schoolbus.WS.CcwFacade/Service/Config.cs
@@ -1,10 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
 
 namespace SchoolBus.WS.CCW.Facade.Service
 {
