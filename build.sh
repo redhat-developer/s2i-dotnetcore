1ea74e80e0958467890d63665c814973c59f471b
Tom Deseyn <tom.deseyn@gmail.com>
2021-01-26 14:20:18 +0100

    build.sh: don't build 5.0 for CentOS because there are no packages for it yet.

--- build.sh
+++ build.sh
@@ -109,7 +109,7 @@ if [ -z "${IMAGE_OS}" ]; then
 fi
 
 if [ "$IMAGE_OS" = "CENTOS" ]; then
-  VERSIONS="${VERSIONS:-2.1 3.1 5.0}"
+  VERSIONS="${VERSIONS:-2.1 3.1}"
   image_postfix="-centos7"
   image_prefix="dotnet"
   docker_filename="Dockerfile"
