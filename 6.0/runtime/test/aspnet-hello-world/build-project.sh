b028b78c051c05d922bac92db319afbd0fc89d9f
Tom Deseyn <tom.deseyn@gmail.com>
2021-09-07 14:48:23 +0200

    Rename 5 to 6

--- 6.0/runtime/test/aspnet-hello-world/build-project.sh
+++ 6.0/runtime/test/aspnet-hello-world/build-project.sh
@@ -2,6 +2,6 @@
 
 rm -rf bin obj
 
-dotnet publish -f net5.0 -c Release
+dotnet publish -f net6.0 -c Release
 
-tar -czvf app.tar.gz -C bin/Release/net5.0/publish .
+tar -czvf app.tar.gz -C bin/Release/net6.0/publish .
