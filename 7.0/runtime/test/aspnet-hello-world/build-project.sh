c583489d199a58cb54a0a86c8e6c944774b0b743
Andrew Slice <andrew.slice@redhat.com>
2022-09-29 11:54:39 -0400

    .NET 7.0 initial
    This includes using the MS build images and test updates.
    NOTE: The precompiled packages for the runtime tests have only been
    rebuilt for x86_64 and not other architectures.

--- 7.0/runtime/test/aspnet-hello-world/build-project.sh
+++ 7.0/runtime/test/aspnet-hello-world/build-project.sh
@@ -2,6 +2,6 @@
 
 rm -rf bin obj
 
-dotnet publish -f net6.0 -c Release
+dotnet publish -f net7.0 -c Release
 
-tar -czvf app-$(uname -m).tar.gz -C bin/Release/net6.0/publish .
+tar -czvf app-$(uname -m).tar.gz -C bin/Release/net7.0/publish .
